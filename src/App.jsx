0bba8ba4741d7c28e27941751acadedb0cad4c41
yadamdeekshitha <yadamdeekshitha@gmail.com>
2026-02-13 18:35:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+function App(){
+  return(
+    <div className='app'>
+      <p>Name: Yadam Deekshitha Naidu <br></br>
+      Id: 2500030277<br></br>
+      Course:CSE<br></br>
+      Year:2025</p>
+
+    </div>
+  );
+
+}
+export default App;
